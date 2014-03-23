3ab5d9da69b0038be9963d2dbcd3a37914dad44c
Junya Wako <j.wako@j-q.co.jp>
2014-03-23 15:40:16 +0900

    add token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sakuran::Application.config.secret_key_base = '1d9936dd5a008f5e2486fb6525f72a5a58b78a62332069960b6e5f8385b6d4e311975ea78558e70768e473f281a8eb402aa7d10b3148be2aed1410452d6792de'
