275ce1de90e2d1ae5cf616084fbc8ab8c4077d0f
Junya Wako <j.wako@j-q.co.jp>
2014-03-30 17:00:41 +0900

    Add right part

--- app/api/twitter_client.rb
+++ app/api/twitter_client.rb
@@ -5,7 +5,15 @@ module TwitterClient
 
 		desc "Returns books based on browse node id from Amazon"
 		get '/tweets', :rabl => "item" do
-			@tweets = Tweet.all
+			ne_lat = params[:ne_lat].blank? ? "" : params[:ne_lat].to_f
+			sw_lat = params[:sw_lat].blank? ? "" : params[:sw_lat].to_f
+			ne_lng = params[:ne_lng].blank? ? "" : params[:ne_lng].to_f
+			sw_lng = params[:sw_lng].blank? ? "" : params[:sw_lng].to_f
+			if ne_lat.present? && sw_lat.present? && ne_lng.present? && sw_lng.present?
+				@tweets = Tweet.where(lat: sw_lat..ne_lat, lon: sw_lng..ne_lng).order(:id)
+			else
+				@tweets = Tweet.order(:id)
+			end
 		end
 	end
 end
--- app/assets/javascripts/controllers/map_controllers.js
+++ app/assets/javascripts/controllers/map_controllers.js
@@ -3,12 +3,40 @@ var mapControllers = angular.module('mapControllers', ['google-maps']);
 mapControllers.controller('MapCtrl', function($scope, $http) {
 	$scope.items = [];
 	$scope.map = {
+		control:{},
     center: {
         latitude: 35.4138,
         longitude: 139.4505
     },
     zoom: 6,
-    dragging: true
+    dragging: true,
+    events: {
+	    dragend: function () {
+        $scope.items = [];
+        var map = $scope.map.control.getGMap();
+        var bounds = map.getBounds();
+		    map_ne_lat = bounds.getNorthEast().lat();
+		    map_sw_lat = bounds.getSouthWest().lat();
+		    map_ne_lng = bounds.getNorthEast().lng();
+		    map_sw_lng = bounds.getSouthWest().lng();
+		    var gallery_scope = angular.element('#gallery').scope();
+		    gallery_scope.items = [];
+		    $http.get('/v1/tweets?ne_lat='+map_ne_lat+'&sw_lat='+map_sw_lat+'&ne_lng='+map_ne_lng+'&sw_lng='+map_sw_lng).success(function(data) {
+					for (var i = 0; i < data.length; i++) {
+				  	$scope.items.push({ 
+				  		latitude: data[i].lat, 
+				  		longitude: data[i].lon, 
+				  		text: data[i].text, 
+				  		url: data[i].url, 
+				  		icon: '/assets/flower.png' 
+				  	});
+				  	gallery_scope.items.push({
+				  		image_url: data[i].url
+				  	})
+				  }
+			  });
+			}
+    }
 	};
 	$http.get('/v1/tweets').success(function(data) {
 		for (var i = 0; i < data.length; i++) {
@@ -20,4 +48,8 @@ mapControllers.controller('MapCtrl', function($scope, $http) {
 	  	});
 	  }
   });
+});
+
+mapControllers.controller('GalleryCtrl', function($scope, $http) {
+	$scope.items = [];
 });
